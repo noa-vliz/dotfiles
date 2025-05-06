3513e747babff77363e7442da778033757e3ff20
noa-vliz <chocolatey.noa@gmail.com>
2025-05-06 20:21:25 +0900

    fix copy_files()

--- .shell/copy_dotfiles.sh
+++ .shell/copy_dotfiles.sh
@@ -4,19 +4,15 @@ home_dir="$HOME"
 
 
 function copy_files(){
+
 	rsync -av \
-  		--include='/.*/' \
-  		--include='/.*' \
+  		--exclude='/.git/' \
+  		--exclude='/.shell/' \
+  		--include='/**.*/' \
+  		--include='/**/.*' \
   		--exclude='*' \
-  		--exclude='.git/' \
   		"$installer_dir/" "$home_dir/"
 
-	mkdir -p "$home_dir/.fluxbox"
-	rsync -av \
-  		--exclude='.' \
-  		--exclude='..' \
-		"$installer_dir/.fluxbox" "$home_dir/.fluxbox"
-	
 	sudo rsync -av \
 		"$installer_dir/scripts/" "/usr/local/bin/"
 }
