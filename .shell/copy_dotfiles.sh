f7b5fe91373cb0edf11f790001aa56b4df7a3b1b
noa-vliz <chocolatey.noa@gmail.com>
2025-05-06 20:51:59 +0900

    fix

--- .shell/copy_dotfiles.sh
+++ .shell/copy_dotfiles.sh
@@ -1,21 +1,20 @@
-
 installer_dir="$(pwd)"
 home_dir="$HOME"
-
-
+ 
+ 
 function copy_files(){
+    rsync -av --inplace --ignore-times \
+        --exclude='/.git/' \
+        --exclude='/.git/**' \
+        --exclude='/.shell/' \
+        --exclude='/.shell/**' \
+        --include='/**/.*' \
+        --include='/**/.*/**' \
+        --exclude='*' \
+        "$installer_dir/" "$home_dir/"
 
-	rsync -av --inplace --ignore-times \
-  		--exclude='/.git/' \
-  		--exclude='/.shell/' \
-  		--include='/**.*/' \
-  		--include='/**/.*' \
-  		--exclude='*' \
-  		"$installer_dir/" "$home_dir/"
-
-	sudo rsync -av --inplace --ignore-times \
-		"$installer_dir/scripts/" "/usr/local/bin/"
+    sudo rsync -av --inplace --ignore-times \
+        "$installer_dir/scripts/" "/usr/local/bin/"
 }
-
 
 
